83d6261d0f9da7d9a9c5e6df8a1c3371fe4711b5
ldin <ldinka.alena@gmail.com>
2016-02-01 22:44:06 +0300

    first commit

---
+++ admin/config-dist.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://flowers.ldinkaea.bget.ru/admin/');
+define('HTTP_CATALOG', 'http://flowers.ldinkaea.bget.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://flowers.ldinkaea.bget.ru/admin/');
+define('HTTPS_CATALOG', 'http://flowers.ldinkaea.bget.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/admin/');
+define('DIR_SYSTEM', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/');
+define('DIR_LANGUAGE', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/config/');
+define('DIR_IMAGE', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/image/');
+define('DIR_CACHE', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ldinkaea_flowers');
+define('DB_PASSWORD', 'gfhjkm');
+define('DB_DATABASE', 'ldinkaea_flowers');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config-dist.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://flowers.ldinkaea.bget.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://flowers.ldinkaea.bget.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/catalog/');
+define('DIR_SYSTEM', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/');
+define('DIR_LANGUAGE', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/config/');
+define('DIR_IMAGE', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/image/');
+define('DIR_CACHE', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/l/ldinkaea/flowers.ldinkaea.bget.ru/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ldinkaea_flowers');
+define('DB_PASSWORD', 'gfhjkm');
+define('DB_DATABASE', 'ldinkaea_flowers');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
